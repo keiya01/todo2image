c699a67548a5281c81045e2f64f301959de06bdc
keiya01 <keiya.s.0210@gmail.com>
2019-04-15 18:30:51 +0900

    Fix styling in Editor of draft.js

--- src/components/pages/CreateImageArea.tsx
+++ src/components/pages/CreateImageArea.tsx
@@ -1,15 +1,33 @@
 import * as React from "react";
 import styled from "styled-components";
+import { EditorState, Editor } from "draft-js";
+
+const { useState } = React;
 
 const Container = styled.div`
-  width: 100vw;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 100%;
   height: 100vh;
+  padding: 10px;
+  cursor: text;
 `;
 
-const CreateImageArea = () => {
+const CreateImageArea: React.FC = () => {
+  const [editorState, setEditorState] = useState(EditorState.createEmpty());
+
+  const handleOnChange = (editorState: EditorState) => {
+    setEditorState(editorState);
+  }
+
   return (
     <Container>
-      Hello World
+      <Editor
+        editorState={editorState}
+        onChange={handleOnChange}
+      />
     </Container>
   )
 };
--- stories/screen.stories.tsx
+++ stories/screen.stories.tsx
@@ -2,7 +2,9 @@ import * as React from "react";
 import { storiesOf } from "@storybook/react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import HomeTopScreen from '../src/components/pages/HomeTopScreen';
+import CreateImageArea from '../src/components/pages/CreateImageArea';
 import "../src/index.css";
 
 storiesOf("Screen", module)
-  .add("HomeTopScreen", () => <Router><Route component={HomeTopScreen} /></Router>);
+  .add("HomeTopScreen", () => <Router><Route component={HomeTopScreen} /></Router>)
+  .add("CreatImageArea", () => <Router><Route component={CreateImageArea} /></Router>);
