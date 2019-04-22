2bf40e4f1986fcef28cac732f820acefb45e24c8
keiya01 <keiya.s.0210@gmail.com>
2019-04-23 08:29:48 +0900

    Add background-color to CreateImageArea.tsx

--- src/components/organisms/EditorHeader.tsx
+++ src/components/organisms/EditorHeader.tsx
@@ -10,7 +10,6 @@ const Header = styled.div`
   position: fixed;
   top: 0;
   right: 0;
-  bottom: 0;
   left: 0;
   z-index: 100;
   background-color: rgba(0, 0, 0, 0.7);
--- src/components/pages/CreateImageArea.tsx
+++ src/components/pages/CreateImageArea.tsx
@@ -20,6 +20,7 @@ const Wrapper = styled.div`
   width: 100%;
   height: 100vh;
   padding: 10px;
+  background-color: #fff;
   cursor: text;
 `;
 
