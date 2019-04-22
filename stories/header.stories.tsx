c714f181447e1b3b49b143d3e3a48d4b053f0c1d
keiya01 <keiya.s.0210@gmail.com>
2019-04-22 15:49:42 +0900

    Fix error in header.stories.tsx

--- stories/header.stories.tsx
+++ stories/header.stories.tsx
@@ -2,5 +2,7 @@ import * as React from "react";
 import {storiesOf} from "@storybook/react";
 import EditorHeader from "../src/components/organisms/EditorHeader";
 
+const handleOnClick = () => {}
+
 storiesOf("Header", module)
-.add("EditorHeader", () => <EditorHeader/>);
+.add("EditorHeader", () => <EditorHeader onClick={handleOnClick}/>);
